6b1c8560e1e12097d39091490aea74214b7fe86d
Nora123-321 <nora.terral@gmail.com>
2025-04-03 22:25:05 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,12 +6,11 @@ function changeCouleur() {
     paragraphe.classList.add("rouge");
 }
 
-document.getElementById('calculate').addEventListener('click', function() {
+document.getElementById('calculate').addEventListener('change', function() {
     let num1 = parseFloat(document.getElementById('num1').value);
     let num2 = parseFloat(document.getElementById('num2').value);
     let operation = document.getElementById('operation').value;
     let result;
         result = num1 * num2;
-}
 document.getElementById('result').textContent = 'Résultat : ' + result;
 });
