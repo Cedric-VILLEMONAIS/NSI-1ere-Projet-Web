6d573fe22bcd941acda9f81f7f19621b06dbd475
Nora123-321 <nora.terral@gmail.com>
2025-04-05 09:26:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,3 +14,30 @@ document.getElementById('calculate').addEventListener('change', function() {
         result = num1 * num2;
 document.getElementById('result').textContent = 'Résultat : ' + result;
 });
+
+document.getElementById('addtomatoes').addEventListener("click", function() {
+    let taskInput = document.getElementById('taskInput');
+    let taskText = taskInput.value;
+
+    taskInput.value = '';
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    let completeButton = document.createElement("button");
+    completeButton.textContent = 'Compléter';
+    completeButton.addEventListener('click', function() {
+        li.style.textDecoration = 'line-through';
+    });
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = 'Supprimer';
+    deleteButton.addEventListener('click', function() {
+        li.remove();
+    });
+
+    li.appendChild(completeButton);
+    li.appendChild(deleteButton);
+
+    document.getElementById("taskList").appendChild(li);
+});
