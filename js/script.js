7e6e007d14fa32d0d74d9ba7e5dc54674c7e99a5
Nora123-321 <nora.terral@gmail.com>
2025-04-12 23:47:37 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 document.getElementById("alerte").addEventListener("click", function() {
-    alert("Attention ! Vous devez bien respecter l'unité des éléments que vous rentrez dans le tableau !" <br> "Vos résultats peuvent être faussés !");
+    alert("Attention ! Vous devez bien respecter l'unité des éléments que vous rentrez dans le tableau ! Vos résultats peuvent être faussés !");
 });
 function changeCouleur() {
     var paragraphe = document.getElementById("important");
     paragraphe.classList.add("rouge");
+}
+
+document.getElementById('addtomatoes');{
+    float: left
 }
 
 document.getElementById('num1','num2').addEventListener('change', function() {
