1af17d62e25bd8f8cadc8cf395b69901494abf69
Nora123-321 <nora.terral@gmail.com>
2025-04-25 12:11:07 +0200

    nv

---
+++ js/scripttwo.js
@@ -0,0 +1,8 @@
+var div = document.getElementById("important");
+div.addEventListener("mouseover", function() {
+    div.style.color = "red";
+});
+var div = document.getElementById("important");
+div.addEventListener("click", function() {
+    div.style.color = "green";
+});
