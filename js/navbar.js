c688e2844bdf520451b90b44b8af5e73b6f272d0
Cédric VILLEMONAIS <cedric.villemonais@gmail.com>
2025-04-27 15:44:23 +0200

    Commentaires navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,12 @@
-/* */
+/* Script pour la barre de navigation */
+
+// Quand tout le site web est chargé on exécute :
 document.addEventListener('DOMContentLoaded', () => {
+    // On importe le bouton mobile et les ul
     const mobile_button = document.querySelector(".mobile-button");
     const navbar_ul = document.querySelector("header>ul")
 
+    // Quand on clique sur le bouton mobile on change l'état de la class active
     mobile_button.addEventListener('click', () => {
         mobile_button.classList.toggle("active");
         navbar_ul.classList.toggle("active")
