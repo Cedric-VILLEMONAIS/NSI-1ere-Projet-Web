47528437bdeb7aee00355d4f3e3c0371ede89cbe
Nora123-321 <nora.terral@gmail.com>
2025-04-27 15:55:36 +0200

    Update page3.php

--- page3.php
+++ page3.php
@@ -8,6 +8,7 @@ require "php/head.php";
 <h1>Les petits calculs pour des achats sûrs</h1>
 <br><!-- retour à la ligne -->
 
+<!-- création tableau de calculs avec l'id "calculs-table" -->
 <table id="calculs-table">
     <caption id="calculs-table-title"> Tableau de calcul </caption>
     <tbody id="calculs-table-tbody">
@@ -33,29 +34,36 @@ require "php/head.php";
             <td class="no-print"></td>
         </tr>
     </tbody>
-</table> 
+</table> <!-- fin du tableau -->
 
-<br><br>
+<br><br><!-- 2 retours à la ligne -->
+<!-- création d'une case (ensemble de champs) avec des choix possibles ayant comme id "calculs-livraison-container" et comme classe "no-print" pour ne pas apparaître lors de l'impression de la page -->
 <fieldset id="calculs-livraison-container" class="no-print">
+  <!-- mettre ce texte en légende de la case -->
   <legend>Voulez-vous être livrés ?</legend>
 
+  <!-- début d'une balise div -->
   <div>
+    <!-- mettre l'option oui de type radio, avec l'id 'Livraison-oui' et le nom 'livraison'. Elle sera automatiquement cochée. Puis ont met le texte à côté de l'option et on ajoute le bouton avec l'id 'taxes' pour ajouter les frais de livraison -->
     <input type="radio" id="livraison-oui" name="livraison" checked/>
     <label for="livraison-oui">Oui, voir le prix final avec les frais de livraison</label>
     <button id="taxes">Ajouter 7,50 €</button>
-  </div>
+  </div> <!-- fin de div -->
 
+   <!-- début d'une balise div -->
   <div>
+    <!-- mettre l'option oui de type radio, avec l'id 'Livraison-non' et le nom 'livraison'. Elle sera automatiquement décochée. Puis ont met le texte à côté de l'option -->
     <input type="radio" id="livraison-non" name="livraison" />
     <label for="livraison-non">Non, c'est bien comme ça</label>
-  </div>
+  </div><!-- fin de div -->
   
-</fieldset>
+</fieldset><!-- fin de l'ensemble des champs -->
 
-<br><br>
+<br><br><!-- 2 retours à la ligne -->
+<!-- création d'un bouton pour imprimer la page -->
 <button onclick="window.print();" id="calculs-print-button" class="no-print">Imprimer la page</button>
 
-<br><br>
+<br><br><!-- 2 retours à la ligne -->
 <!-- création d'un bouton avec un lien pour retourner à la page principale rapidement -->
 <button id="button-return-home" class="no-print"><a href="index.php">Revenir à la page principale</a></button>
 
